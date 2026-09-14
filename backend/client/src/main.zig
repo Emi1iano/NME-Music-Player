4dcc946cd913874ff3002618d97c682ae348dabb
Emi1iano <emilano.bocanegra01@utrgv.edu>
2026-09-14 15:43:33 -0500

    udp stuff

--- backend/client/src/main.zig
+++ backend/client/src/main.zig
@@ -2,7 +2,28 @@ const std = @import("std");
 const Io = std.Io;
 
 pub fn main(init: std.process.Init) !void {
-    const ip = try std.Io.net.IpAddress.parse("192.168.0.62", 5252);
-    const client = try ip.connect(init.io, .{ .mode = .stream });
-    defer client.close(init.io);
+    const alloc = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(alloc);
+    defer alloc.free(args[0..]);
+
+
+    const client = try Io.net.IpAddress.parse("0.0.0.0", getPortFromArg(args[1]));
+    const server = try std.Io.net.IpAddress.parse("24.243.26.72", 5252);
+    const client_socket = try client.bind(init.io, .{ .mode = .dgram });
+    defer client_socket.close(init.io);
+
+    try client_socket.send(init.io, &server, "12345678");
+
+    var buffer: [1024]u8 = undefined;
+    while (true) {
+        const message = try client_socket.receive(init.io, &buffer);
+        std.debug.print("{s}\n", .{message.data});
+    }
+}
+fn getPortFromArg(buffer: [:0]const u8) u16 {
+    var result: u16 = 0;
+    for (buffer) |c| switch (c) {
+        '0'...'9' => result = result * 10 + c - '0',
+        else => {},
+    } else return result;
 }
--- backend/server/src/main.zig
+++ backend/server/src/main.zig
@@ -2,8 +2,51 @@ const std = @import("std");
 const Io = std.Io;
 
 const MAX_CONNECTIONS: usize = 16;
-const CLIENTS: [MAX_CONNECTIONS]std.Io.net.Stream = undefined;
+const Connection = struct {key: [8]u8 = undefined, ip: std.Io.net.IpAddress, timestamp: std.Io.Timestamp};
+var CLIENTS: [MAX_CONNECTIONS]?Connection = [_]?Connection{null} ** MAX_CONNECTIONS;
+var CLIENTS_SIZE: usize = 0;
+const CONNECTIONS = struct {
+    fn add(conn: Connection) !void {
+        for (CLIENTS) |client| {
+            if (client == null) continue;
+            if (client.?.ip.eql(&conn.ip)) return error.ClientAlreadyWaiting;
+        }
+        for (&CLIENTS) |*client| {
+            if (client.* == null) {
+                client.* = conn;
+                CLIENTS_SIZE += 1;
+                return;
+            }
+        } else {
+            return error.TooManyClients;
+        }
+    }
+    fn cleanUp() !void {
 
+    }
+    fn pairUp(io: Io) !void {
+        for (0..CLIENTS.len) |x| {
+            for (x+1..CLIENTS.len) |y| {
+                if (CLIENTS[x] == null or CLIENTS[y] == null) continue;
+                if (std.mem.eql(u8, &CLIENTS[x].?.key, &CLIENTS[y].?.key)) {
+                    const client1 = CLIENTS[x].?.ip;
+                    const client2 = CLIENTS[y].?.ip;
+                    std.debug.print("{any}: {any}\n", .{client1, client2});
+                    const client1_socket = try client1.bind(io, .{ .mode = .dgram });
+                    const client2_socket = try client2.bind(io, .{ .mode = .dgram });
+
+                    
+
+                    try client1_socket.send(io, &client2, &CLIENTS[y].?.key);
+                    try client2_socket.send(io, &client1, &CLIENTS[x].?.key);
+
+                    client1_socket.close(io);
+                    client2_socket.close(io);
+                }
+            }
+        } 
+    }
+};
 // Wait for 2 clients to connect with the same password/key
 // Connect them to each other
 pub fn main(init: std.process.Init) !void {
@@ -14,16 +57,28 @@ pub fn main(init: std.process.Init) !void {
 fn mainThread(init: std.process.Init) !void {
     const ip = try std.Io.net.IpAddress.parse("192.168.0.62", 5252);
     std.debug.print("Server Opened...\n", .{});
-    var server = try ip.listen(init.io, .{ .mode = .stream });
-    defer server.deinit(init.io);
+    var server_socket = try ip.bind(init.io, .{ .mode = .dgram });
+    defer server_socket.close(init.io);
 
+    var buffer: [1024]u8 = undefined;
     while (true) {
-        var client = try server.accept(init.io);
+        const message = try server_socket.receive(init.io, &buffer);
         
-        std.debug.print("{any} Connected", .{client.socket.address.ip4});
-        client.close(init.io);
+        if (message.data.len == 8) {
+            const b = message.from.ip4.bytes;
+            std.debug.print("{d}.{d}.{d}.{d}:{d} Connected with key: {s}\n", .{b[0], b[1], b[2], b[3], message.from.getPort(), message.data});
+
+            var conn = Connection {.ip = message.from, .timestamp = std.Io.Timestamp.now(init.io, .awake)};
+            @memcpy(&conn.key, message.data[0..8]);
+            try CONNECTIONS.add(conn);
+            try CONNECTIONS.pairUp(init.io);
+        }
     }
 }
 fn workerThread(init: std.process.Init) !void {
-    _ = init;
+    while (true) {
+        std.debug.print("size: {d}\n", .{CLIENTS_SIZE});
+
+        try init.io.sleep(.fromSeconds(1), .awake);
+    }
 }
