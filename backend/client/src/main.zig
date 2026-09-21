edd5ffedb140a1b0668a105b47153dcf835196d3
Emi1iano <emilano.bocanegra01@utrgv.edu>
2026-09-21 12:50:26 -0500

    added same nat connectivity

--- backend/client/src/main.zig
+++ backend/client/src/main.zig
@@ -14,7 +14,19 @@ pub fn main(init: std.process.Init) !void {
     const args = try init.minimal.args.toSlice(alloc);
     defer alloc.free(args[0..]);
 
-    if (args.len == 1) return error.InvalidArgs;
+    _ = try getLocalIpWindows(init.io);
+    
+    if (args.len == 1) {
+        if (SYNCING.readKey(init.io)) |key| {
+            std.debug.print("Found key: {s}\n", .{key});
+            try SYNCING.start(init, key, 0);
+        } else |_| {
+            const key = try SYNCING.generateNewKey(init.io);
+            std.debug.print("Couldn't find key. Generated new key: {s}\n", .{key});
+            try SYNCING.start(init, key, 0);
+        }
+        return error.InvalidArgs;
+    }
     if (std.mem.eql(u8, args[1], "sync")) {
         if (args.len == 2) {
             if (SYNCING.readKey(init.io)) |key| {
@@ -34,7 +46,7 @@ pub fn main(init: std.process.Init) !void {
 
             std.debug.print("Connecting with key: {s}\n", .{key});
             try SYNCING.writeKey(init.io, key);
-            //try SYNCING.start(init, args[2]);
+            try SYNCING.start(init, key, 0);
         }
     } else if (std.mem.eql(u8, args[1], "sync_new_key")) {
         std.debug.print("Generating new key\n", .{});
@@ -44,31 +56,34 @@ pub fn main(init: std.process.Init) !void {
         try SYNCING.start(init, try SYNCING.readKey(init.io), SYNCING.getPortFromArg(args[1]));
     }
 }
-
-fn cin(io: std.Io, buffer: []u8) []u8 {
-    var rBuffer: [256]u8 = undefined;
-    var stdin = std.Io.File.stdin().reader(io, &rBuffer);
-    var input = &stdin.interface;
-
-    var writer = std.Io.Writer.fixed(buffer);
-
-    const len = input.streamDelimiter(&writer, '\n') catch 1;
-
-    if (builtin.os.tag == .windows) {
-        return buffer[0 .. len - 1];
-    } else return buffer[0..len];
-}
-
 const SYNCING = struct {
     pub fn start(init: std.process.Init, key: [8]u8, port: u16) !void {
-
         const client_port: u16 = if (port == 0) 32145 else port;
-        const client = try Io.net.IpAddress.parse("0.0.0.0", client_port);
+        var client = try Io.net.IpAddress.parse("0.0.0.0", client_port);
         const server = try std.Io.net.IpAddress.parse("24.243.26.72", 5252);
-        const client_socket = try client.bind(init.io, .{ .mode = .dgram });
+        var client_socket: std.Io.net.Socket = undefined;
+        while (true) {
+            if (client.bind(init.io, .{ .mode = .dgram })) |socket| {
+                client_socket = socket;
+                break;
+            } else |err| switch (err) {
+                error.AddressInUse => {
+                    client.setPort(client.ip4.port+1);
+                },
+                else => {return err;},
+            }
+        }
         defer client_socket.close(init.io);
 
-        try client_socket.send(init.io, &server, &key);
+        const local_ip_bytes = try getLocalIp(init.io);
+        const local_ip = try formatIp(local_ip_bytes, client_socket.address.getPort());
+        std.debug.print("Opening on local port: {d}\n", .{client_socket.address.getPort()});
+        
+        //SEND 6 BYTES FOR LOCAL IP FOLLOWED BY 8 BYTES OF THE KEY
+        var info: [14]u8 = undefined;
+        @memcpy(info[0..6], &local_ip);
+        @memcpy(info[6..14], &key);
+        try client_socket.send(init.io, &server, &info);
 
         var buffer: [1024]u8 = undefined;
         var message = try client_socket.receive(init.io, &buffer);
@@ -80,7 +95,6 @@ const SYNCING = struct {
         const other_client_port = getPortFromServer(message.data[4..6]);
         const other_client: std.Io.net.IpAddress = .{ .ip4 = .{ .bytes = bytes, .port = other_client_port } };
         
-        //add a connected message
         std.debug.print("Connecting with {d}.{d}.{d}.{d}:{d}\n", .{bytes[0], bytes[1], bytes[2], bytes[3], other_client_port});
 
         const thread = try std.Thread.spawn(.{}, workerThread, .{init.io, client_socket});
@@ -89,7 +103,8 @@ const SYNCING = struct {
             try client_socket.send(init.io, &other_client, "PUNCH");
             try init.io.sleep(.fromMilliseconds(200), .awake);
         }
-
+        
+        
         while (true) {
             const s = cin(init.io, &buffer);
             try client_socket.send(init.io, &other_client, s);
@@ -100,6 +115,7 @@ const SYNCING = struct {
         var buffer: [1024]u8 = undefined;
         while (true) {
             const message = try socket.receive(io, &buffer);
+            if (std.mem.eql(u8, message.data, "PUNCH")) continue;
             std.debug.print("{s}\n", .{message.data});
         }
     }
@@ -165,3 +181,70 @@ const SYNCING = struct {
         return result;
     }
 };
+fn cin(io: std.Io, buffer: []u8) []u8 {
+    var rBuffer: [256]u8 = undefined;
+    var stdin = std.Io.File.stdin().reader(io, &rBuffer);
+    var input = &stdin.interface;
+
+    var writer = std.Io.Writer.fixed(buffer);
+
+    const len = input.streamDelimiter(&writer, '\n') catch 1;
+
+    if (builtin.os.tag == .windows) {
+        return buffer[0 .. len - 1];
+    } else return buffer[0..len];
+}
+fn getLocalIp(io: Io) ![4]u8 {
+    switch (builtin.os.tag) {
+        .windows => {
+            return try getLocalIpWindows(io);
+        },
+        else => {
+            const result: [4]u8 = [_]u8{0} ** 4; 
+            return result;
+        }
+    }
+}
+fn getLocalIpWindows(io: std.Io) ![4]u8 {
+    var result: [4]u8 = .{192, 168, 0, 0};
+    var child = try std.process.spawn(io, .{ .argv = &.{"ipconfig"}, .stdout = .pipe });
+    
+    var buffer: [1024]u8 = undefined;
+    var wbuffer: [1024]u8 = undefined;
+    var reader = child.stdout.?.reader(io, &buffer);
+    var input = &reader.interface;
+    var writer = std.Io.Writer.fixed(&wbuffer);
+
+    _ = try input.streamRemaining(&writer);
+    const result1 = std.mem.cut(u8, writer.buffered(), "192.168.");
+    const result2 = std.mem.cut(u8, result1.?.@"1", "\n");
+
+    var byte: u8 = 0;
+    var x: usize = 0;
+    for (result2.?.@"0") |c| {
+        switch (c) {
+            '0'...'9' => {
+                byte = byte * 10 + (c - '0');
+            },
+            else => {
+                result[2+x] = byte;
+                x+=1;
+            }
+        }
+    }
+
+    _ = try child.wait(io);
+    return result;
+}
+fn formatIp(bytes: [4]u8, port: u16) ![6]u8 {
+    var result: [6]u8 = [_]u8{0} ** 6;
+    @memcpy(result[0..4], &bytes);
+
+    result[4] |= @truncate(port >> 8);
+    result[5] |= @truncate(port);
+
+    return result;
+}
+fn formatIpAddress(ip: std.Io.net.IpAddress) ![6]u8 {
+    return formatIp(ip.ip4.bytes, ip.ip4.bytes);
+}
--- backend/server/src/main.zig
+++ backend/server/src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const Io = std.Io;
 
 const MAX_CONNECTIONS: usize = 16;
-const Connection = struct {key: [8]u8 = undefined, ip: std.Io.net.IpAddress, timestamp: std.Io.Timestamp};
+const Connection = struct {key: [8]u8 = undefined, ip: std.Io.net.IpAddress, local_ip: ?std.Io.net.IpAddress = null, timestamp: std.Io.Timestamp};
 var CLIENTS: [MAX_CONNECTIONS]?Connection = [_]?Connection{null} ** MAX_CONNECTIONS;
 var CLIENTS_SIZE: usize = 0;
 const CONNECTIONS = struct {
@@ -29,9 +29,19 @@ const CONNECTIONS = struct {
             for (x+1..CLIENTS.len) |y| {
                 if (CLIENTS[x] == null or CLIENTS[y] == null) continue;
                 if (std.mem.eql(u8, &CLIENTS[x].?.key, &CLIENTS[y].?.key)) {
-                    const client1 = CLIENTS[x].?.ip;
-                    const client2 = CLIENTS[y].?.ip;
-                    
+                    var client1: std.Io.net.IpAddress = undefined;
+                    var client2: std.Io.net.IpAddress = undefined;
+                    if (std.mem.eql(u8, &CLIENTS[x].?.ip.ip4.bytes, &CLIENTS[y].?.ip.ip4.bytes)) {
+                        if (CLIENTS[x].?.local_ip == null or CLIENTS[y].?.local_ip == null) {
+                            return error.NoLocalIp;
+                        }
+                        client1 = CLIENTS[x].?.local_ip.?;
+                        client2 = CLIENTS[y].?.local_ip.?;
+                    } else {
+                        client1 = CLIENTS[x].?.ip;
+                        client2 = CLIENTS[y].?.ip;
+                    }
+                    //TODO: make this not needed
                     var buffer: [6]u8 = undefined;
 
                     try server_spcket.send(io, &client1, formatIp(client2, &buffer));
@@ -64,23 +74,42 @@ fn mainThread(init: std.process.Init) !void {
     while (true) {
         const message = try server_socket.receive(init.io, &buffer);
         
-        if (message.data.len == 8) {
+        if (message.data.len == 14) {
+            const local_ip = bufToIp(message.data[0..6].*);
             const b = message.from.ip4.bytes;
-            std.debug.print("{d}.{d}.{d}.{d}:{d} Connected with key: {s}\n", .{b[0], b[1], b[2], b[3], message.from.getPort(), message.data});
+            const key = message.data[6..14];
+            try print(init.io, "\x1b[1A{d}.{d}.{d}.{d}:{d} Connected with key: {s}\x1b[1E\n", .{b[0], b[1], b[2], b[3], message.from.getPort(), key});
 
             var conn = Connection {.ip = message.from, .timestamp = std.Io.Timestamp.now(init.io, .awake)};
-            @memcpy(&conn.key, message.data[0..8]);
-            try CONNECTIONS.add(conn);
+            @memcpy(&conn.key, key);
+
+            const aux: u32 = @bitCast(local_ip.ip4.bytes);
+            if (aux != 0) conn.local_ip = local_ip; 
+
+            CONNECTIONS.add(conn) catch |err| switch (err) {
+                error.ClientAlreadyWaiting => {
+                    try print(init.io, "\x1b[1A{any}\x1b[1E\n", .{err});
+                    continue;
+                },
+                else => {},
+            };
             try CONNECTIONS.pairUp(init.io, &server_socket);
         }
     }
 }
 fn workerThread(init: std.process.Init) !void {
+    std.debug.print("size: {d}\n", .{CLIENTS_SIZE});
     while (true) {
-        std.debug.print("size: {d}\n", .{CLIENTS_SIZE});
+        try print(init.io, "\x1b[1Asize: {d}\n", .{CLIENTS_SIZE});
 
         try init.io.sleep(.fromSeconds(1), .awake);
     }
+}
+var lock = std.Io.Mutex.init;
+fn print(io: Io, comptime fmt: []const u8, args: anytype) !void {
+    try lock.lock(io);
+    std.debug.print(fmt, args);
+    lock.unlock(io);
 }
 fn formatIp(ip: std.Io.net.IpAddress, buffer: []u8) []u8 {
     @memcpy(buffer[0..4], &ip.ip4.bytes);
@@ -90,6 +119,14 @@ fn formatIp(ip: std.Io.net.IpAddress, buffer: []u8) []u8 {
     buffer[4] |= @truncate(port >> 8);
     buffer[5] |= @truncate(port);
 
-    std.debug.print("{b:0>16} : {b:0>8}{b:0>8}\n", .{ip.ip4.port, buffer[4], buffer[5]});
+    //std.debug.print("{b:0>16} : {b:0>8}{b:0>8}\n", .{ip.ip4.port, buffer[4], buffer[5]});
     return buffer;
+}
+fn bufToIp(bytes: [6]u8) std.Io.net.IpAddress {
+    //.{bytes[0], bytes[1], bytes[2], bytes[3]}
+    var port: u16 = 0;
+    port |= bytes[4];
+    port <<= 8;
+    port |= bytes[5];
+    return .{ .ip4 = .{ .bytes = bytes[0..4].*, .port = port } };
 }
